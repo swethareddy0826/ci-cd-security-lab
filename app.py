67756a2601efb5d83f7e37a827ab49585bb31b85
Nalla swetha <nallaswetha9390@gmail.com>
2026-03-14 23:29:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import requests
 
-# fake API key for security detection
 API_KEY = "123456SECRETKEY"
 
 def get_data():
     url = "https://api.github.com"
-    response = requests.get(url)
+    response = requests.get(url, timeout=5)
     return response.status_code
 
 print(get_data())
